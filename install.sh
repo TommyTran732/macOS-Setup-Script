f72ec845f13e0d319dfac5fb2fcee3afbb0b0ee2
Tommy <contact@tommytran.io>
2022-04-09 05:50:09 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/autoupdate
+brew install gnupg pinentry-mac exiftools
+brew install --cask brave-browser docker gimp github google-drive ivpn lens lyx onlyoffice spotify visual-studio-code wireshark
+
