3437ac3461565f0295df7d3b4d92c6cc21da21cb
Tommy <contact@tommytran.io>
2022-04-18 01:25:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install gnupg pinentry-mac exiftools
-brew install --cask brave-browser docker gimp github google-drive ivpn lens lyx onlyoffice spotify visual-studio-code wireshark yubico-manager
+brew install gnupg pinentry-mac exiftools gpgme
+brew install --cask brave-browser docker gimp github google-drive ivpn lens lyx onlyoffice spotify visual-studio-code wireshark yubico-manager thunderbird
