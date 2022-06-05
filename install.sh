00248c923c02a0e6a6cbbbb4c7b1b10037dbfa5f
Tommy <contact@tommytran.io>
2022-06-05 06:00:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ defaults write com.apple.mail EnableBundles -bool true
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
 brew install free-gpgmail
-brew install --cask docker github google-drive ivpn lens spotify visual-studio-code
+brew install --cask brave-browser docker github protonvpn lens visual-studio-code
