f73ec8d7f888852ca6f61898c7a80e0fa490934e
Tommy <contact@tommytran.io>
2022-06-06 01:53:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 defaults write com.apple.mail EnableBundles -bool true
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install free-gpgmail
+brew install free-gpgmail android-platform-tools
 brew install --cask brave-browser docker github protonvpn lens visual-studio-code
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
