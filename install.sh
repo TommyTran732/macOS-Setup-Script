418e7314b7cc85b76cabab6355accf64697a2c8a
Tommy <contact@tommytran.io>
2022-06-17 06:25:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ defaults write com.apple.mail EnableBundles -bool true
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
 brew install free-gpgmail android-platform-tools
-brew install --cask brave-browser docker element github protonvpn lens visual-studio-code
+brew install --cask brave-browser docker element google-drive github ivpn lens visual-studio-code yubikey-manager
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
+echo "export HOMEBREW_NO_ANALYTICS=1" >> ~/.zshrc
 
 #Comment this line out if on Intel Mac
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
