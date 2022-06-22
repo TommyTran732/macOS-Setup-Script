0acadc22ab5e1f3a0c473bc5504c0b2b7e3cbe7e
Tommy <contact@tommytran.io>
2022-06-22 02:41:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ defaults write com.apple.mail EnableBundles -bool true
 echo "export HOMEBREW_NO_ANALYTICS=1" >> ~/.zshrc
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
+
 #Comment this line out if on Intel Mac
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
 brew install free-gpgmail android-platform-tools
