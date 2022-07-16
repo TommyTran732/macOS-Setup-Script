56df335e9044ddd5f62a201743f8ef3fb16a026b
Tommy <contact@tommytran.io>
2022-07-16 14:46:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ defaults write com.apple.mail EnableBundles -bool true
 echo "export HOMEBREW_NO_ANALYTICS=1" >> ~/.zshrc
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
+echo 'export PATH="$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.zshrc
 
 #Comment this line out if on Intel Mac
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install free-gpgmail android-platform-tools firebase-cli hugo
+brew install free-gpgmail android-platform-tools firebase-cli hugo gnu-sed
 brew install --cask android-studio brave-browser cryptomator docker element google-drive github ivpn onlyoffice raspberry-pi-imager visual-studio-code yubico-yubikey-manager wireshark
-brew install --default-names gnu-sed 
