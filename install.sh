ba91b0e9bed2d955f17ccdaac875e8ca32e16be6
Tommy <contact@tommytran.io>
2022-09-22 21:34:08 -0400

    Remove unnecessary packages
    Signed-off-by: Tommy <contact@tommytran.io>

--- install.sh
+++ install.sh
@@ -4,20 +4,11 @@ defaults write com.apple.mail EnableBundles -bool true
 echo "export HOMEBREW_NO_ANALYTICS=1" >> ~/.zshrc
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
-echo 'export PATH="$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.zshrc
-echo 'export PATH="$(brew --prefix)/opt/openssl@3/bin:$PATH"' >> ~/.zshrc
 echo 'export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-echo 'export CPPFLAGS="-I/usr/local/opt/openjdk/include"' >> ~/.zshrc
 
 #Comment this line out if on Intel Mac
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install butane gnu-sed hugo java openssl
-brew install --cask android-studio docker element free-gpgmail github microsoft-auto-update microsoft-edge mullvadvpn visual-studio-code
-
-#Add app cleaner
-curl --fail --output /usr/local/bin/app-cleaner.sh https://raw.githubusercontent.com/sunknudsen/privacy-guides/master/how-to-clean-uninstall-macos-apps-using-appcleaner-open-source-alternative/app-cleaner.sh
-chmod +x /usr/local/bin/app-cleaner.sh
+brew install --cask android-studio docker free-gpgmail github mullvadvpn visual-studio-code
