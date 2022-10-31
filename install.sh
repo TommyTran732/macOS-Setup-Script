95c847a7f503038b82fb6080c544367f5251b372
Tommy <contact@tommytran.io>
2022-10-31 18:58:01 -0400

    Update package list
    Signed-off-by: Tommy <contact@tommytran.io>

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ defaults write com.apple.mail EnableBundles -bool true
 echo "export HOMEBREW_NO_ANALYTICS=1" >> ~/.zshrc
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
-echo 'export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"' >> ~/.zshrc
 
 #Comment this line out if on Intel Mac
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
@@ -14,4 +13,4 @@ awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo > /
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install --cask android-studio docker github gpg-suite microsoft-edge mullvadvpn parallels raspberry-pi-imager visual-studio-code
+brew install --cask android-platform-studio docker github gpg-suite microsoft-auto-update microsoft-edge mullvadvpn raspberry-pi-imager visual-studio-code
