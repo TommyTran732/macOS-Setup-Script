3bc5cd09f2a9b6e0bdd40f4f28f77b6a6e66b4b1
Tommy <contact@tommytran.io>
2022-11-03 17:59:02 -0400

    Remove android tools and rasp imager
    Signed-off-by: Tommy <contact@tommytran.io>

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo > /
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/autoupdate
-brew install --cask android-platform-studio docker github gpg-suite microsoft-auto-update microsoft-edge mullvadvpn raspberry-pi-imager visual-studio-code
+brew install --cask docker github gpg-suite microsoft-auto-update microsoft-edge mullvadvpn visual-studio-code
