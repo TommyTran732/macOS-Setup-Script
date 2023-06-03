80f63bc026b98184492cfdecdc7793b5f92f9346
Tommy <contact@tommytran.io>
2023-06-02 19:41:21 -0700

    Hide administrator user
    Signed-off-by: Tommy <contact@tommytran.io>

--- administrator.sh
+++ administrator.sh
@@ -6,3 +6,7 @@ awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | s
 # Verify SSH Fingerprints
 echo "VerifyHostKeyDNS yes" | sudo tee /etc/ssh/ssh_config.d/10-custom.conf
 sudo chmod 644 /etc/ssh/ssh_config.d/10-custom.conf
+
+# Hide the administrator user
+sudo dscl . create /Users/administrator IsHidden 1
+sudo chflags hidden /Users/administrator
