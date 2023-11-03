f74e1cf8b6dd5d5f4b2b5deba9b78b9f580f240a
Tommy <contact@tommytran.io>
2023-11-03 15:37:39 -0700

    Update development.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- development.sh
+++ development.sh
@@ -12,5 +12,6 @@ defaults write com.apple.Safari IncludeInternalDebugMenu 1
 
 #Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/autoupdate
+brew tap homebrew/cask-versions
 brew install --cask android-platform-tools chronycontrol github gpg-suite-no-mail microsoft-edge mullvadvpn orbstack visual-studio-code
-  
