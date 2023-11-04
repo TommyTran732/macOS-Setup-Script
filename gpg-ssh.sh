c5a7a5b25a4208fd6a7d9529d50c0c6e0ced74e6
Tommy <contact@tommytran.io>
2023-11-04 13:13:31 -0700

    Update GPG config
    Signed-off-by: Tommy <contact@tommytran.io>

--- gpg-ssh.sh
+++ gpg-ssh.sh
@@ -1,2 +1,4 @@
 echo "gpgconf --launch gpg-agent" >> ~/.zshrc
 echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
+echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf
+echo "08106E4A223469E096995A44B8EC0E5BB58C643C" >> ~/.gnupg/sshcontrol
