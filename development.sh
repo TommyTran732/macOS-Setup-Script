bc80cf463c80f00bb4675718cda833e8b5680592
Tommy <contact@tommytran.io>
2023-11-11 14:51:43 -0700

    Protect home
    Signed-off-by: Tommy <contact@tommytran.io>

--- development.sh
+++ development.sh
@@ -3,6 +3,9 @@
 # Comment this line out if you don't have Touch ID
 awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo
 
+# Protect Home
+sudo chmod 700 /Users/tomster
+
 # Verify SSH Fingerprints
 echo "VerifyHostKeyDNS yes" | sudo tee /etc/ssh/ssh_config.d/10-custom.conf
 sudo chmod 644 /etc/ssh/ssh_config.d/10-custom.conf
