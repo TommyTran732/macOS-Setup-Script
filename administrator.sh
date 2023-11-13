a05f5d2af6d07b412fd95d1cb7a588db857789a9
Tommy <contact@tommytran.io>
2023-11-13 13:22:32 -0700

    Update Home protection command
    Signed-off-by: Tommy <contact@tommytran.io>

--- administrator.sh
+++ administrator.sh
@@ -4,7 +4,9 @@
 awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo
 
 # Protect Home
-sudo chmod 700 /Users/administrator
+for user in $(ls /Users | grep -v 'Shared'); do
+sudo chmod 700 /Users/$user
+done
 
 # Verify SSH Fingerprints
 echo "VerifyHostKeyDNS yes" | sudo tee /etc/ssh/ssh_config.d/10-custom.conf
