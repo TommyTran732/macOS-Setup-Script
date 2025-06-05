a85831d06ca8a060a953ff8e9fa22d91772f5bd1
Tommy <contact@tommytran.io>
2025-06-05 04:54:37 -0400

    Delete gpg-ssh.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- gpg-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# shellcheck disable=SC1071
-
-echo "gpgconf --launch gpg-agent" >> ~/.zshrc
-echo "export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh" >> ~/.zshrc
-echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf
-echo "08106E4A223469E096995A44B8EC0E5BB58C643C" >> ~/.gnupg/sshcontrol
