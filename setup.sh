c5024063cee20c6d8c653f67ce57660c9d9aca85
Tommy <contact@tommytran.io>
2025-07-06 18:02:56 -0700

    Typo Fixes & Block all connections
    Signed-off-by: Tommy <contact@tommytran.io>

--- setup.sh
+++ setup.sh
@@ -23,19 +23,20 @@ sudo launchctl remove /System/Library/LaunchDaemons/org.cups.cupsd.plist
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/libexec/remoted
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/bin/python3
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/bin/ruby
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/libexec/remoted
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/sbin/cupsd
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/libexec/sharingd
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/libexec/sshd-keygen-wrapper
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/sbin/cupsd
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --blockapp /usr/sbin/smbd
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setblockall all
 
 # Setup Edge Enterprise Policies
 sudo mkdir -p '/Library/Tomster Corporation/scripts/' '/Library/Tomster Corporation/prefs/' '/Library/Managed Preferences'
-curl https://raw.githubusercontent.com/TommyTran732/macOS-Setup-Script/etc/Library/Tomster%20Corporation/apply_prefs.sh | sudo tee '/etc/Library/Tomster Corporation/apply_prefs.sh'
+curl https://raw.githubusercontent.com/TommyTran732/macOS-Setup-Script/Library/Tomster%20Corporation/apply_prefs.sh | sudo tee '/Library/Tomster Corporation/apply_prefs.sh'
 sudo chmod 744 '/Library/Tomster Corporation/scripts/apply_prefs.sh'
 curl https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/macOS/Managed%20Preferences/com.microsoft.Edge.plist | sudo tee '/Library/Tomster Corporation/prefs/com.microsoft.Edge.plist'
 curl https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/macOS/Preferences/com.microsoft.Edge.plist | sudo tee /Library/Preferences/com.microsoft.Edge.plist
-curl https://raw.githubusercontent.com/TommyTran732/macOS-Setup-Script/main/etc/Library/LaunchDaemons/io.tommytran.prefs.plist | sudo tee /etc/Library/LaunchDaemons/io.tommytran.prefs.list
+curl https://raw.githubusercontent.com/TommyTran732/macOS-Setup-Script/main/Library/LaunchDaemons/io.tommytran.prefs.plist | sudo tee /Library/LaunchDaemons/io.tommytran.prefs.list
 sudo launchctl load -w /Library/LaunchDaemons/io.tommytran.prefs.plist
